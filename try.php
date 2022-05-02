02c0f34cc3aa98522ea136eedb88bd7bd252ea82
mohit <mk7738771@gmail.com>
2022-05-02 13:18:32 +0530

    introduction

---
+++ try.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction of mohit</title>
+</head>
+<body>
+    <h3>Introduction of mohit</h3>
+    <p>my name is Mohit kumar singh
+        i am pursuing bca
+    </p>
+
+</body>
+</html>
